ffaf1ec0f46cce8dbad88b28d712a9b36deabed3
Adolfo Brunno <adolfo.brunno.ba@gmail.com>
2026-03-07 18:40:54 -0300

    Adicionando HealthcheckController para verificação de status e ativando Lombok em Reply e Button, além de ajustar configuração de mínimo de máquinas ativas para 1 no Fly.io

--- src/main/java/com/whatsapp/api/domain/messages/Button.java
+++ src/main/java/com/whatsapp/api/domain/messages/Button.java
@@ -1,23 +1,17 @@
 package com.whatsapp.api.domain.messages;
 
 import com.whatsapp.api.domain.messages.type.ButtonType;
+import lombok.Getter;
 
+@Getter
 public class Button {
 
     private ButtonType type;
     private Reply reply;
 
-    public ButtonType getType() {
-        return type;
-    }
-
     public Button setType(ButtonType type) {
         this.type = type;
         return this;
-    }
-
-    public Reply getReply() {
-        return reply;
     }
 
     public Button setReply(Reply reply) {
--- src/main/java/com/whatsapp/api/domain/messages/Reply.java
+++ src/main/java/com/whatsapp/api/domain/messages/Reply.java
@@ -1,21 +1,16 @@
 package com.whatsapp.api.domain.messages;
 
+import lombok.Getter;
+
+@Getter
 public class Reply {
 
     private String id;
     private String title;
 
-    public String getId() {
-        return id;
-    }
-
     public Reply setId(String id) {
         this.id = id;
         return this;
-    }
-
-    public String getTitle() {
-        return title;
     }
 
     public Reply setTitle(String title) {
